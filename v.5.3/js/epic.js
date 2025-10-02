43c4799b9b6ad4e895fa8f3615bc78469f832376
Markides.Markellos <markellos.markides@gmail.com>
2025-10-02 22:32:53 +0300

    Update epic.js

--- v.5.3/js/epic.js
+++ v.5.3/js/epic.js
@@ -41,10 +41,10 @@ document.addEventListener("DOMContentLoaded", () => {
   function buildEpicSentence(locus, colorW, pieceAssocW, sanW, targetAssocW,
                              colorB, pieceAssocB, sanB, targetAssocB, anchor) {
     const templates = [
-      `${locus} όπου βλέπει το λευκό στρατό με ${pieceAssocW}, και την κίνηση ${sanW}, ... ${targetAssocW}, και τότε ο μαύρος στρατός με ${pieceAssocB} αντιδρά, με την κίνηση ${sanB} ... και τον βλέπει να ${targetAssocB}.`,
-      `${locus} όπου βλέπει το λευκό στρατό με ${pieceAssocW}, και την κίνηση ${sanW}, ... ${targetAssocW}, και τότε ο μαύρος στρατός με ${pieceAssocB} απαντά, με την κίνηση ${sanB}, ... και τον βλέπει να ${targetAssocB}.`,
-      `${locus} όπου βλέπει το λευκό στρατό με ${pieceAssocW}, και την κίνηση ${sanW}, ... ${targetAssocW}, αλλά ο μαύρος στρατός με ${pieceAssocB} απαντά, με την κίνηση ${sanB} ... και τον βλέπει να ${targetAssocB}.`,
-      `${locus} όπου βλέπει το λευκό στρατό με ${pieceAssocW}, και την κίνηση ${sanW}, ... ${targetAssocW}, αλλά ο μαύρος στρατός με ${pieceAssocB} αντιδρά, με την κίνηση ${sanB}, ... και τον βλέπει να ${targetAssocB}.`
+      `${locus} όπου βλέπει το λευκό στρατό με ${pieceAssocW}, και την κίνηση ${sanW}, ... ${targetAssocW}, και τότε ο μαύρος στρατός με ${pieceAssocB} αντιδρά, με την κίνηση ${sanB} ... και να ${targetAssocB}.`,
+      `${locus} όπου βλέπει το λευκό στρατό με ${pieceAssocW}, και την κίνηση ${sanW}, ... ${targetAssocW}, και τότε ο μαύρος στρατός με ${pieceAssocB} απαντά, με την κίνηση ${sanB}, ... και να ${targetAssocB}.`,
+      `${locus} όπου βλέπει το λευκό στρατό με ${pieceAssocW}, και την κίνηση ${sanW}, ... ${targetAssocW}, αλλά ο μαύρος στρατός με ${pieceAssocB} απαντά, με την κίνηση ${sanB} ... και να ${targetAssocB}.`,
+      `${locus} όπου βλέπει το λευκό στρατό με ${pieceAssocW}, και την κίνηση ${sanW}, ... ${targetAssocW}, αλλά ο μαύρος στρατός με ${pieceAssocB} αντιδρά, με την κίνηση ${sanB}, ... και να ${targetAssocB}.`
     ];
     let sentence = templates[Math.floor(Math.random() * templates.length)];
 
@@ -176,6 +176,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
 
 
 
