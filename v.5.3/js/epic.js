879709ba35536ed96c9fb8e0221f2f91b9a41a40
Markides.Markellos <markellos.markides@gmail.com>
2025-10-12 22:20:15 +0300

    Update epic.js

--- v.5.3/js/epic.js
+++ v.5.3/js/epic.js
@@ -63,9 +63,9 @@ function buildEpicSentence(
   const vB = verbsB[Math.floor(Math.random() * verbsB.length)];
   const link = links[Math.floor(Math.random() * links.length)];
 
-  let sentence = `Σκηνή ${locus}. ${vW} ${pieceAssocW} ${targetAssocW}, με την κίνηση ${sanW}. Και τότε, ${pieceAssocB} ${vB} ${targetAssocB}, με την κίνηση ${sanB}.`;
+  let sentence = `Σκηνή ${locus}. ${vW} ${pieceAssocW} ${targetAssocW}, με την κίνηση ${sanW}. Και τότε, ${pieceAssocB} ${vB} και ${targetAssocB}, με την κίνηση ${sanB}.`;
 
-  if (anchor) sentence += ` Και τότε ξάφνου μπροστά τους παρουσιάζεται ${anchor}.`;
+  if (anchor) sentence += ` Και τότε ξάφνου μπροστά τους εμφανίζεται ${anchor}.`;
 
   return sentence;
 }
@@ -190,6 +190,7 @@ const gameHeader = `${event}\n${white} vs ${black}\n${formattedDate}`.trim(); //
     if (event.target === modal) modal.style.display = "none";
   });
 });
+
 
 
 
