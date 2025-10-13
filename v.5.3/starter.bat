c9527707a8889022996f5695aeb510522ce1f017
Markides.Markellos <markellos.markides@gmail.com>
2025-10-13 10:24:10 +0300

    Delete v.5.3/starter.bat

--- v.5.3/starter.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Εκκίνηση τοπικού server με Python (πρέπει να υπάρχει εγκατεστημένος Python)
-cd /d "%~dp0"
-start "" http://localhost:8080/index.html
-python -m http.server 8080
-pause
