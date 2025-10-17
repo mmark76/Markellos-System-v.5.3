c2a877fa391fb18b490bb1f6e5a248046e92c26d
Markides.Markellos <markellos.markides@gmail.com>
2025-10-17 12:01:26 +0300

    Refactor epic.js to improve modal functionality

--- v.5.3/js/epic.js
+++ v.5.3/js/epic.js
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const sanText = sanToText(san);
 
       let sceneNumber = i + 1; // αριθμός σκηνής
-      let phrase = `Σκηνή ${sceneNumber}: \n `;
+      let phrase = `Σκηνή ${sceneNumber}: \n\n `;
       phrase += ` ${locus}, ξεπροβάλλει ${pieceAssoc}, και ${targetAssoc}, με την κίνηση ${sanText}.\n`;
  
       if (anchorTxt) phrase = `${anchorTxt}\n\n${phrase}`;
@@ -158,6 +158,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (event.target === modal) modal.style.display = "none";
   });
 });
+
 
 
 
