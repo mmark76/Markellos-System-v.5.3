41007d0e2968043445e9fcd41ebf1a6faf7fa84c
Markides.Markellos <markellos.markides@gmail.com>
2025-10-25 08:48:40 +0300

    Update epic.js

--- js/epic.js
+++ js/epic.js
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const sanText = sanToText(san);
 
 const openings = [
-  "Στη συνέχεια..., ο Γέροντας συνεχίζει την αφήγηση...,",
+  "Ο Γέροντας συνεχίζει την αφήγηση...,",
   "Λίγο αργότερα..., και καθώς η μάχη συνεχίζεται...,",
   "Μετά από λίγο..., και καθώς ο μικρός σκακιστής συνεχίζει να παρακολουθεί τη μάχη με μεγάλη αγωνία...,",
 ];
@@ -78,12 +78,12 @@ const verbs = [
   "διακρίνεται στο πεδίο της μάχης,"
 ];
 
-const opening = i === 0 ? " Ο Γέροντας ξεκινάει την αφήγηση και διαβάζει ... Η μάχη ξεκινάει αργά το απόγευμα. Οι δύο Στρατηγοί δίνουν τα χέρια, ακούγεται μία σάλπιγγα και" : openings[i % openings.length];
+const opening = i === 0 ? "Ο Γέροντας ξεκινάει την αφήγηση και διαβάζει ... Η μάχη ξεκινάει αργά το απόγευμα. Οι δύο Στρατηγοί δίνουν τα χέρια, ακούγεται μία σάλπιγγα και" : openings[i % openings.length];
 const action = verbs[i % verbs.length];
 
 let sceneNumber = i + 1;
 const sanLabel = `${sceneNumber}. ${sanText}`;
-const t1Header = `♞♟ Ημικίνηση ${sanLabel}. Η σκηνή όπου εμφανίζεται ${locus} και ${pieceAssoc}.`;
+const t1Header = `♞♟ Ημικίνηση ${sanLabel}. Η σκηνή όπου εμφανίζονται ${locus} και ${pieceAssoc}.`;
 let phrase = `${t1Header}\n\n ${opening} ${action} ${locus}..., και τότε ${pieceAssoc} ${targetAssoc}.\n`;
 if (anchorTxt) phrase = `${anchorTxt}\n${phrase}`;
 
@@ -174,6 +174,7 @@ stories.push(phrase.trim());
     if (event.target === modal) modal.style.display = "none";
   });
 });
+
 
 
 
