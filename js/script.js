b3e9c2275af2387aca52fc5dc75dd76040246535
Markides.Markellos <markellos.markides@gmail.com>
2025-10-28 14:36:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -412,7 +412,7 @@ function buildLibrariesBar(){
 
 /* ---------- Init ---------- */
 async function loadLibraries(){
-  const res = await fetch('libraries_2810251435.json');
+  const res = await fetch('libraries_v.5.3.json');
   libs = await res.json();
   console.log("LIBS KEYS:", Object.keys(libs));
   console.log("Temporal:", libs.Temporal);
