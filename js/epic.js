51de75518dca0d2acf0454de247244110c7a199d
Markides.Markellos <markellos.markides@gmail.com>
2025-10-30 21:46:32 +0200

    Update epic.js

--- js/epic.js
+++ js/epic.js
@@ -89,7 +89,7 @@ const opening = i === 0 ? "Η μάχη ξεκινάει αργά το απόγε
 const action = verbs[i % verbs.length];
 
 let sceneNumber = i + 1;
-const t1Header = `Half-move ${sceneNumber}. ${sanText}:\n - Σκηνή: Στο πεδίο της μάχης, στην περιοχή ${square} εμφανίζεται ${locus}`;
+const t1Header = `Half-move ${sceneNumber}. ${sanText}: ${targetAssoc} \n - Σκηνή: Στο πεδίο της μάχης, στην περιοχή ${square} εμφανίζεται ${locus}`;
 let phrase = `${t1Header} \n\n ${opening} ${action} ${pieceAssoc}.\n`;
 if (anchorTxt) phrase = `${anchorTxt} \n ${phrase}`;
 
@@ -180,6 +180,7 @@ stories.push(phrase.trim());
     if (event.target === modal) modal.style.display = "none";
   });
 });
+
 
 
 
