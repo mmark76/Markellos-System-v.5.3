4953f1821f2ef4eae238c08393c071f025a89134
Markides.Markellos <markellos.markides@gmail.com>
2025-10-31 23:17:45 +0200

    Update epic.js

--- js/epic.js
+++ js/epic.js
@@ -90,7 +90,7 @@ const action = verbs[i % verbs.length];
 
 let sceneNumber = i + 1;
 const t1Header = `Half-move ${sceneNumber}. ${sanText}.\n - ${opening} στην περιοχή ${square}, όπου είναι ${targetAssoc}. Και ξάφνου, λίγο παρά δίπλα ${action} ${locus}.`;
-let phrase = `${t1Header} Στη δράση του ${square} ορμά ${pieceAssoc}.`;
+let phrase = `${t1Header} Τότε στη δράση του ${square} ορμά ${pieceAssoc}.`;
 if (anchorTxt) phrase = `${anchorTxt} ${phrase}`;
 
 stories.push(phrase.trim());
@@ -180,6 +180,7 @@ stories.push(phrase.trim());
     if (event.target === modal) modal.style.display = "none";
   });
 });
+
 
 
 
