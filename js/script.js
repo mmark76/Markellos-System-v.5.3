2db1a6109b2df250ebeea3f97c007f7369a47203
Markides.Markellos <markellos.markides@gmail.com>
2025-11-08 16:53:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -326,8 +326,7 @@ function fillAssociationsTable(moves){
       `<td>${escapeHtml(m.to)}</td>`+
       `<td>${escapeHtml(sideGR(m.side))}</td>`+
       `<td>${escapeHtml(pieceAssoc)}</td>`+
-      `<td>${escapeHtml(targetAssoc)}</td>`+   // <-- νέα ξεχωριστή στήλη
-      `<td>${escapeHtml(storyText)}</td>`;
+      `<td>${escapeHtml(targetAssoc)}</td>`+  
 
     body.appendChild(tr);
   });
