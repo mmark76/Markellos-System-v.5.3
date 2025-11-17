a2fee9e994abcf70dff5a3a262f4884365c85011
Markides.Markellos <markellos.markides@gmail.com>
2025-11-17 13:55:07 +0200

    Add files via upload

---
+++ js/structured-data.js
@@ -0,0 +1,56 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "SoftwareApplication",
+  "name": "Chess Mnemonic Application and Epic Chess Stories Creator",
+  "alternateName": "Chess Mnemonic System v3.3",
+  "version": "3.3",
+  "description": "A specialized memory training tool for chess players that converts PGN/SAN into mnemonic tables, PAO sequences, Rhythm structures and memory palace loci.",
+  "applicationCategory": "EducationalApplication",
+  "softwareHelp": "https://markellos-chess-mnemonic-system.blogspot.com/",
+  "operatingSystem": "Web",
+  "url": "https://chessmnemonics.net/index.html",
+  "featureList": [
+    "PGN/SAN Parser",
+    "Associations Table",
+    "PAO 0–9 and PAO 00–99 Tables",
+    "Rhythm Tables",
+    "Memory Palace Mode",
+    "Epic Story Generator",
+    "Flashcards Training Module"
+  ],
+  "softwareAddOn": {
+    "@type": "SoftwareApplication",
+    "name": "Chess Mnemonics Flashcards Trainer",
+    "url": "https://chessmnemonics.net/flashcards/"
+  },
+  "keywords": [
+    "chess mnemonics",
+    "memory palace",
+    "pao system",
+    "chess training",
+    "chess notation",
+    "epic story",
+    "san to mnemonic",
+    "memory techniques",
+    "PGN visualization"
+  ],
+  "author": {
+    "@type": "Person",
+    "name": "Markellos Markides",
+    "url": "https://markellos-chess-mnemonic-system.blogspot.com/"
+  },
+  "creator": {
+    "@type": "Person",
+    "name": "Markellos Markides"
+  },
+  "license": "© Markellos Markides 2025",
+  "copyrightHolder": "Markellos Markides",
+  "inLanguage": "en",
+  "learningResourceType": "Mnemonic-based training",
+  "publisher": {
+    "@type": "Organization",
+    "name": "Chess Mnemonics"
+  }
+}
+</script>
