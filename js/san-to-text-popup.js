938ad0ee01700fb1e06eafcf50ccafa6883829e8
Markides.Markellos <markellos.markides@gmail.com>
2025-11-18 15:54:42 +0200

    Update san-to-text-popup.js

--- js/san-to-text-popup.js
+++ js/san-to-text-popup.js
@@ -189,8 +189,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if (mode === "half") {
               for (let i=0;i<moves.length;i++){
-            const side = moves[i].side === "w" ? "White" : "Black";
-              out.push("Half-move " + (i+1) + " (" + side + "): " + sanToTextInner(moves[i].san) + ".");
+                const side = moves[i].side === "w" ? "White" : "Black";
+                out.push("Half-move " + (i+1) + " (" + side + "): " + sanToTextInner(moves[i].san) + ".");
 
               }
             } else {
@@ -312,6 +312,7 @@ document.addEventListener("DOMContentLoaded", () => {
   setTimeout(enableSanButtonIfReady, 200);
 
 });
+
 
 
 
